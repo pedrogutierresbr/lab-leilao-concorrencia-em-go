89a241688379d043c64f0058408b5b66af1ea862
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-03-26 09:13:58 -0300

    implement auction use case with creation and retrieval functionality

--- internal/entity/auction_entity/auction_entity.go
+++ internal/entity/auction_entity/auction_entity.go
@@ -1,6 +1,42 @@
 package auction_entity
 
-import "time"
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pedrogutierresbr/lab-leilao-concorrencia-em-go/internal/internal_error"
+)
+
+func CreateAuction(productName, category, description string, condition ProductCondition) (*Auction, *internal_error.InternalError) {
+	auction := &Auction{
+		Id:          uuid.New().String(),
+		ProductName: productName,
+		Category:    category,
+		Description: description,
+		Condition:   condition,
+		Status:      Active,
+		Timestamp:   time.Now(),
+	}
+
+	if err := auction.Validate(); err != nil {
+		return nil, err
+	}
+
+	return auction, nil
+}
+
+func (au *Auction) Validate() *internal_error.InternalError {
+	if len(au.ProductName) <= 1 ||
+		len(au.Category) <= 2 ||
+		len(au.Description) <= 10 && (au.Condition != New &&
+			au.Condition != Refurbished &&
+			au.Condition != Used) {
+		return internal_error.NewBadRequestError("invalid auction object")
+	}
+
+	return nil
+}
 
 type Auction struct {
 	Id          string
@@ -25,3 +61,9 @@ const (
 	Active AuctionStatus = iota
 	Completed
 )
+
+type AuctionRepositoryInterface interface {
+	CreateAuction(ctx context.Context, auctionEntity *Auction) *internal_error.InternalError
+	FindAuctions(ctx context.Context, status AuctionStatus, category, productName string) ([]Auction, *internal_error.InternalError)
+	FindAuctionById(ctx context.Context, id string) (*Auction, *internal_error.InternalError)
+}
