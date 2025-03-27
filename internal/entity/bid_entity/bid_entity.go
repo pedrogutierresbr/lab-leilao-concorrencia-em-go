7cf7c50f189533aa2a1b75e9ffe2b735822d79ec
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-03-27 20:22:48 -0300

    implement batch processing for bid creation with validation and environment configuration

--- internal/entity/bid_entity/bid_entity.go
+++ internal/entity/bid_entity/bid_entity.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/pedrogutierresbr/lab-leilao-concorrencia-em-go/internal/internal_error"
 )
 
@@ -13,6 +14,34 @@ type Bid struct {
 	AuctionId string
 	Amount    float64
 	Timestamp time.Time
+}
+
+func CreateBid(userId, auctionId string, amount float64) (*Bid, *internal_error.InternalError) {
+	bid := &Bid{
+		Id:        uuid.New().String(),
+		UserId:    userId,
+		AuctionId: auctionId,
+		Amount:    amount,
+		Timestamp: time.Now(),
+	}
+
+	if err := bid.Validate(); err != nil {
+		return nil, err
+	}
+
+	return bid, nil
+}
+
+func (b *Bid) Validate() *internal_error.InternalError {
+	if err := uuid.Validate(b.UserId); err != nil {
+		return internal_error.NewBadRequestError("UserId is not a valid id")
+	} else if err := uuid.Validate(b.AuctionId); err != nil {
+		return internal_error.NewBadRequestError("AuctionId is not a valid id")
+	} else if b.Amount <= 0 {
+		return internal_error.NewBadRequestError("Amount is not a valid value")
+	}
+
+	return nil
 }
 
 type BidEntityRepository interface {
